610f0d1de766b8ac2c26cb423160c830b1d5e0d3
DaisyGPD <daisygpd@gmail.com>
2021-04-07 00:51:21 +0900

    WPFの基本実装

---
+++ TextTempleter/TextTempleter/MainWindowViewModel.cs
@@ -0,0 +1,66 @@
+﻿using Prism.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextTempleter
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        private string text;
+
+        private DelegateCommand testCommand;
+
+        public MainWindowViewModel()
+        {
+
+        }
+
+        public string Text
+        {
+            get
+            {
+                return this.text;
+            }
+
+            set
+            {
+                if (this.text == value)
+                {
+                    return;
+                }
+
+                this.text = value;
+                this.OnPropertyChanged(nameof(this.Text));
+            }
+        }
+
+        public DelegateCommand TestCommand
+        {
+            get
+            {
+                if (testCommand == null)
+                {
+                    testCommand = new DelegateCommand(this.Execute, this.CanExecute);
+                }
+
+                return testCommand;
+            }
+        }
+
+        private int count;
+
+        private void Execute()
+        {
+            this.Text = "Hoge : " + count;
+            ++count;
+        }
+
+        private bool CanExecute()
+        {
+            return true;
+        }
+    }
+}
---
+++ TextTempleter/TextTempleter/ViewModelBase.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+
+namespace TextTempleter
+{
+    public class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        
+        /// <summary>
+        /// プロパティ更新
+        /// </summary>
+        /// <param name="propertyName"></param>
+        public void OnPropertyChanged(string propertyName)
+        {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
